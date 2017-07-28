87ced7cc00bb91632669e0c42142c0a673417e5e
Waller Jhonatan <waller@ufv.br>
2017-07-28 08:31:35 -0300

    Altera template email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,43 +3,37 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row" style="padding-left: 50px">
             <div class="col-md-6 col-md-offset-2">
-                <div class="panel panel-default">
+                <div class="box box-info">
                     <div class="box-header with-border">
                         <h3 class="box-title">Redefinição de Senha</h3>
                     </div>
-                    <div class="panel-body">
-                        @if (session('status'))
-                            <div class="alert alert-success">
-                                {{ session('status') }}
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <form action="{{ route('password.email') }}" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+                        <div class="box-body">
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                                <div class="col-sm-10">
+                                    <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Email" required>
+
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                                        <strong>{{ $errors->first('email') }}</strong>
+                                                    </span>
+                                    @endif
+                                </div>
                             </div>
-                        @endif
-                            <form action="{{ route('password.email') }}" method="POST" class="form-group">
-                                {{ csrf_field() }}
-                                <div class="box-body">
-                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-
-                                        <div class="col-sm-10">
-                                            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Email" required>
-
-                                            @if ($errors->has('email'))
-                                                <span class="help-block">
-                                                    <strong>{{ $errors->first('email') }}</strong>
-                                                </span>
-                                            @endif
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary pull-right">Enviar link de redefinição de senha</button>
-                                </div>
-                                </div>
-                                <!-- /.box-footer -->
-                            </form>
-                    </div>
+                        </div>
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-primary pull-right">Enviar link de redefinição de senha</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
